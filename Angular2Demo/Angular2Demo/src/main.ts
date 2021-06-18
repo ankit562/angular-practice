f73628b45cfbb73852a5f982fc74872345fa27eb
ankit562 <76042136+ankit562@users.noreply.github.com>
2021-06-18 15:54:40 +0530

    fsf
    fsfasf

--- Angular2Demo/Angular2Demo/src/main.ts
+++ Angular2Demo/Angular2Demo/src/main.ts
@@ -3,4 +3,3 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
-/// <reference path="../node_modules/angular2/typings/browser.d.ts" />
